dcd2c4f2b1f7253ed7996bc1086f8192121e4cc2
Ben Guetta <95125332+bn-jo@users.noreply.github.com>
2022-03-19 19:51:19 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,51 @@
+const jokeEl = document.getElementById('joke')
+const jokeBtn = document.getElementById('jokeBtn')
+
+jokeBtn.addEventListener('click', generateJoke)
+
+generateJoke()
+
+// USING ASYNC/AWAIT
+async function generateJoke() {
+    const config = {
+        headers: {
+            Accept: 'application/json',
+        },
+    }
+
+    const res = await fetch('https://icanhazdadjoke.com', config)
+
+    const data = await res.json()
+
+    jokeEl.innerHTML = data.joke
+
+    var msg = new SpeechSynthesisUtterance();
+    msg.text = data.joke;
+
+    msg.rate = 0.7;
+    window.speechSynthesis.speak(msg);
+}
+
+// USING .then()
+// function generateJoke() {
+//   const config = {
+//     headers: {
+//       Accept: 'application/json',
+//     },
+//   }
+
+//   fetch('https://icanhazdadjoke.com', config)
+//     .then((res) => res.json())
+//     .then((data) => {
+//       jokeEl.innerHTML = data.joke
+//     })
+// }
+
+// var voices = window.speechSynthesis.getVoices();
+// msg.voice = voices[10];
+// msg.volume = 1; // From 0 to 1
+// msg.rate = 0.6; // From 0.1 to 10
+// msg.pitch = 1; // From 0 to 2
+// msg.text = data.joke;
+// msg.lang = 'en';
+// speechSynthesis.speak(msg);
